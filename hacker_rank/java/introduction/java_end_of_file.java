4753198616a7ed7d76976bbfcaf6c8d3cbe7dedb
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-09-22 15:00:25 -0700

    wrote hacker_rank/java/introduction/java_end_of_file.java

---
+++ hacker_rank/java/introduction/java_end_of_file.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int lineNum = 1;
+
+        while(scan.hasNext() == true) {
+            if (scan.hasNext() == true) {
+                String s = scan.nextLine();
+                System.out.println(lineNum + " " + s);
+                lineNum ++;
+            }
+        }
+
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
