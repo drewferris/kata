d9c09a83b3bfd2dcd615775825c2c7b85676cd05
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-09-29 16:20:34 -0700

    completed next video

---
+++ treehouse/c_sharp/FitnessFrog/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+
+  static void Main()
+  {
+    System.Console.Write("Enter how many minutes you exercised: ");
+    string entry = System.Console.ReadLine();
+
+  }
+
+}
