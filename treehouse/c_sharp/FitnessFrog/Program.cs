232779402f45fe8d004be4144b87165b02dfc48d
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-09-30 15:15:30 -0700

    finished video for refactor

--- treehouse/c_sharp/FitnessFrog/Program.cs
+++ treehouse/c_sharp/FitnessFrog/Program.cs
@@ -7,9 +7,43 @@ namespace Treehouse.FitnessFrog
 
     static void Main()
     {
-      Console.Write("Enter how many minutes you exercised: ");
-      string entry = Console.ReadLine();
-      Console.WriteLine("You've exercised " + entry + " minutes");
+      var runningTotal = 0.0;
+
+      while (true)
+      {
+
+        Console.Write("Enter how many minutes you exercised or type \"quit\" to exit: ");
+        var entry = Console.ReadLine();
+
+        if (entry.ToLower() == "quit")
+        {
+          break;
+        }
+
+          try {
+              var minutes = double.Parse(entry);
+
+              if (minutes <= 0) {
+                Console.WriteLine(minutes + " is an invaled number");
+                continue;
+              } else if (minutes <= 10) {
+                Console.WriteLine("Better than nothing, am I right?");
+              } else if(minutes <= 30) {
+                Console.WriteLine("Way to go hot stuff!");
+              } else if (minutes <= 60) {
+                Console.WriteLine("You must be a ninja warrior in trainint!");
+              } else {
+                Console.WriteLine("Okay, now you're just showing off.");
+              }
+            runningTotal += minutes;
+            }
+          catch (FormatException) {
+            Console.WriteLine("That is not a valid input.");
+            continue;
+          }
+          Console.WriteLine("You've exercised " + runningTotal + " minutes.");
+
+      }
     }
 
   }
