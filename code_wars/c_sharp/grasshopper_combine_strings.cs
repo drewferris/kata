aa31436cc11fc2fe6f2360c5959221a0e61078b0
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-10-02 04:58:10 -0700

    wrote grasshopper_combine_strings.cs

---
+++ code_wars/c_sharp/grasshopper_combine_strings.cs
@@ -0,0 +1,7 @@
+public class Kata {
+  public static string CombineNames(string firstName, string lastName) {
+    return firstName + " " + lastName;
+
+  }
+}
+// Create the CombineNames method here
