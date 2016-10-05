7c4982501e2f4c5260394c1ebe189ecc02bb79df
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-10-05 13:18:10 -0700

    wrote new files for cs demo

--- treehouse/c_sharp/ObjectGameProject/Game.cs
+++ treehouse/c_sharp/ObjectGameProject/Game.cs
@@ -1,10 +1,21 @@
+using System;
+
 namespace TreehouseDefense
 {
     class Game
     {
         public static void Main()
         {
-            Tower tower = new Tower();
+          Map map = new Map(8, 5);
+
+          Point x = new MapLocation(4,2);
+
+          Point p = x;
+
+          map.OnMap(new MapLocation(0,0));
+
+          Console.WriteLine(x.DistanceTo(5,5));
+
         }
     }
 }
