ca443748118dc35716a28845c95608c2ce1a6fcf
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-10-06 17:00:16 -0700

    wrote repeated_stings.js from leaderboard

--- hacker_rank/javascript/algorithms/implementation/repeated_string.js
+++ hacker_rank/javascript/algorithms/implementation/repeated_string.js
@@ -26,3 +26,26 @@ function main() {
     console.log(count);
   }
 }
+//solution found on leaderboard
+
+function main() {
+  var s = readLine().split("");
+  var n = parseInt(readLine());
+
+  //variable equal to string length
+  var stringSize = s.length;
+  //variable equal to the amount of times 'a' occurs in s
+
+  var a = s.filter((a) => a == 'a').length;
+
+  //floored value of amount of times stringSize goes into n
+  var repeat = Math.floor(n/stringSize);
+
+  //number of indices left over when string can no longer completely go into remaining indices
+  var left = n-(repeat*stringSize);
+
+  //return the amount of times 'a' occurs in each string times the amount of times the string occurs before the index plus how many times 'a' occurs in the last string before it exceeds the index value
+  console.log((repeat*a) + s.filter((a,i) => a == 'a' && i < left).length);
+
+
+}
